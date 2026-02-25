92d2488ce357680a1508640df7906420a9d31440
Miklraw <133392464+Miklraw@users.noreply.github.com>
2026-02-25 22:35:04 +0300

    Delete gantt-app directory

--- gantt-app/css/app.js
+++
@@ -1,8 +0,0 @@
-import { store } from './store.js';
-import { Notifier } from './notifications.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Приложение запущено на сервере");
-    Notifier.checkDeadlines();
-    // Тут вызов функций отрисовки Gantt.render() и т.д.
-});
